f226f1cbbe718d19b8f7a4fee76b5778df773ea7
Jara Santamaria <jarasmar@gmail.com>
2020-05-13 13:38:34 +0100

    create file to time shuffle method

--- lib/shuffle_method_time_frame.rb
+++ lib/shuffle_method_time_frame.rb
@@ -1,15 +1,14 @@
 require 'benchmark'
 require 'gchart'
 
-def sort_time_frame
+def shuffle_time_frame
   results = []
   m = 5000
   while m <= 100_000
-    array = Array(1..m).shuffle
-    m += 5_000
     time = Benchmark.measure do
-      array.sort
+      array = Array(1..m).shuffle
     end
+    m += 5_000
     # takes the real time  and changes it from seconds to miliseconds
     results.push(time.real * 1_000)
   end
@@ -20,11 +19,11 @@ generate_graph = Gchart.new(
   :type => 'line',
   :size => '450x500',
   :bar_colors => ['6600cc','b366ff'],
-  :title => "Time Framework for Sort Method",
+  :title => "Time Framework for Shuffle Method",
   :bg => 'EFEFEF',
-  :legend => ['Sort Method'],
-  :data => [sort_time_frame],
-  :filename => 'images/sort_graph.png',
+  :legend => ['Shuffle Method'],
+  :data => [shuffle_time_frame],
+  :filename => 'images/shuffle_method_graph.png',
   :stacked => false,
   :legend_position => 'bottom',
   :axis_with_labels => [['x'], ['y']],
@@ -33,4 +32,4 @@ generate_graph = Gchart.new(
   :axis_labels => [["0|20_000|40_000|60_000|80_000|100_000"]],
   )
 
- generate_graph.file
+  generate_graph.file
