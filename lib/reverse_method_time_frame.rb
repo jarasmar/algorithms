abc79cf2800401e885a1b8763b2bfa47a32a2d5d
Jara Santamaria <jarasmar@gmail.com>
2020-05-18 17:55:57 +0100

    add new method reverse to refactor

--- lib/reverse_method_time_frame.rb
+++ lib/reverse_method_time_frame.rb
@@ -2,7 +2,7 @@ require 'benchmark'
 require 'gchart'
 
 class Array
-  def new_reverse(array)
+  def new_reverse_one(array)
     new_array = []
     l = array.length
       while l > 0
@@ -11,6 +11,18 @@ class Array
       end
     new_array
   end
+
+  def new_reverse_two(array)
+    
+  end
+  
+#   function reverse(arr) {
+#     new_arr = []
+#     for (let i = 0; i < arr.length; i++) {
+#         new_arr.unshift(arr[i])
+#     }
+#     return new_arr;    
+#   }
 end
 
 class Reverse < Array
@@ -29,12 +41,12 @@ class Reverse < Array
     results
   end
 
-  def new_reverse_timer
+  def new_reverse_one_timer
     results = []
     m = 5000
     while m <= 100_000
       time = Benchmark.measure do
-        array = new_reverse(Array(1..m))
+        array = new_reverse_one(Array(1..m))
       end
       m += 5_000
       # takes the real time  and changes it from seconds to miliseconds
@@ -43,22 +55,36 @@ class Reverse < Array
     results
   end
 
-  def new_reverse_two(string)
-    array = string.split(//)
-    new_array = []
-    new_array << array.pop while array.length > 0
-    new_array.join("")
+  def new_reverse_two_timer
+    results = []
+    m = 5000
+    while m <= 100_000
+      time = Benchmark.measure do
+        array = new_reverse_two(Array(1..m))
+      end
+      m += 5_000
+      # takes the real time  and changes it from seconds to miliseconds
+      results.push(time.real * 1_000)
+    end
+    results
   end
+
+  # def new_reverse_two(string)
+  #   array = string.split(//)
+  #   new_array = []
+  #   new_array << array.pop while array.length > 0
+  #   new_array.join("")
+  # end
 
   def graph
     generate_graph = Gchart.new(
       :type => 'line',
       :size => '450x500',
-      :bar_colors => ['6600cc','b366ff'],
+      :bar_colors => ['6600cc','b366ff', '330066'],
       :title => "Time Framework for Reverse Method",
       :bg => 'EFEFEF',
-      :legend => ['New Reverse', 'Original Reverse'],
-      :data => [new_reverse_timer, reverse_original],
+      :legend => ['New Reverse One', 'New Reverse Two', 'Original Reverse'],
+      :data => [new_reverse_one_timer, new_reverse_two_timer, reverse_original],
       :filename => 'images/reverse_method_graph.png',
       :stacked => false,
       :legend_position => 'bottom',
@@ -74,6 +100,3 @@ end
 
 a = Reverse.new
 a.graph
-
-p a.new_reverse_timer
-p a.reverse_original
